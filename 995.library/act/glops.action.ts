0a3a336858ae98965e864b382e0dba353a3adfac
Brad Henderson <elliotbradly@gmail.com>
2025-08-10 14:51:22 -0400

    update

---
+++ 995.library/act/glops.action.ts
@@ -0,0 +1,6 @@
+export const INIT_GLOPS = "[glops action] Init glops";
+export const OPEN_GLOPS = "[glops action] Open glops";
+export const UPDATE_GLOPS = "[glops action] Update glops";
+export const ADVANCE_GLOPS = "[Advance action] Advance glops";
+export const OUTPUT_GLOPS = "[Output action] Output glops";
+export const ACCESS_GLOPS = "[Access action] Access glops";
